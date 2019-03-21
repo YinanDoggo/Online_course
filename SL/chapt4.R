40d73bc920c28d024b2ea2e8fd4ac497fee3bb07
YinanDoggo <zynalc@hotmail.com>
2019-03-22 00:02:14 +0800

    00:02 22.03.2019 @China worked on SL

--- SL/chapt4.R
+++ SL/chapt4.R
@@ -7,3 +7,107 @@ exp(b0+b1*x1+b2*x2)/(1+exp(b0+b1*x1+b2*x2))
 (log(0.5/(1-0.5))-b0-b2*x2)/b1
 
 log(0.05/(1-0.05))
+
+library(ISLR)
+?Smarket
+head(Smarket)
+pairs(Smarket, col=Smarket$Direction)
+attach(Smarket)
+glm.fit = glm(Direction~Lag1+Lag2+Lag3+Lag4+Lag4+Lag5+Volume, data=Smarket, family=binomial)
+summary(glm.fit)
+glm.probs = predict(glm.fit, type="response")        
+glm.probs[1:5]
+glm.pred = ifelse(glm.probs>0.5, "Up","Down")
+table(glm.pred, Direction)
+mean(glm.pred == Direction)
+
+train = Year<2005
+glm.fit = glm(Direction~Lag1+Lag2+Lag3+Lag4+Lag4+Lag5+Volume, data=Smarket, family=binomial,subset=train)
+glm.probs = predict(glm.fit, newdata=Smarket[!train,], type="response")
+glm.pred = ifelse(glm.probs>0.5, "Up","Down")
+Direction.2005 = Smarket$Direction[!train]
+table(glm.pred, Direction.2005)
+mean(glm.pred == Direction.2005)
+
+glm.fit = glm(Direction~Lag1+Lag2, data=Smarket, family=binomial,subset=train)
+glm.probs = predict(glm.fit, newdata=Smarket[!train,], type="response")
+glm.pred = ifelse(glm.probs>0.5, "Up","Down")
+Direction.2005 = Smarket$Direction[!train]
+table(glm.pred, Direction.2005)
+mean(glm.pred == Direction.2005)
+summary(glm.fit)
+
+#linear disciminatn analysis
+library(MASS)
+library(ISLR)
+
+lda.fit = lda(Direction~Lag1+Lag2, data=Smarket, subset=Year<2005)
+lda.fit
+plot(lda.fit)
+Smarket.2005 = subset(Smarket, Year==2005)
+lda.pred = predict(lda.fit, Smarket.2005)
+lda.pred[1:5,]
+class(lda.pred)
+data.frame(lda.pred)[1:5,]
+table(lda.pred$class, Smarket.2005$Direction)
+mean(lda.pred$class == Smarket.2005$Direction)
+
+head(lda.pred$posterior)
+sum(lda.pred$posterior[,1]>=0.5)
+sum(lda.pred$posterior[,1]<0.5)
+sum(lda.pred$class == "Down")
+sum(lda.pred$class == "Up")
+
+lda.pred$posterior[1:20,1]
+lda.pred$class[1:20]
+
+sum(lda.pred$posterior[,1]>0.9)
+max(lda.pred$posterior[,1])
+
+#K-Nearest Neighbors
+library(class)
+?knn
+Xlag = cbind(Lag1,Lag2)
+head(Xlag)
+train=Year<2005
+knn.pred = knn(Xlag[train,], Xlag[!train,], Direction[train], k=1)
+table(knn.pred, Direction[!train])
+train.X = cbind(Lag1,Lag2)[train,]
+test.X = cbind(Lag1,Lag2)[!train,]
+train.Direction = Direction[train]
+Direction.2005 = Direction[!train]
+set.seed(1)
+knn.pred = knn(train.X, test.X, train.Direction, k=1)
+table(knn.pred, Direction.2005)
+
+#Caravan Insurance Data
+library(ISLR)
+summary(Caravan)
+attach(Caravan)
+summary(Purchase)
+standardized.X = scale(Caravan[,-86])
+var(Caravan[,1])
+var(standardized.X[,1])
+
+test = 1:1000
+train.X = standardized.X[-test,]
+test.X = standardized.X[test,]
+train.Y = Purchase[-test]
+test.Y = Purchase[test]
+set.seed(1)
+knn.pred = knn(train.X, test.X, train.Y, k=1)
+mean(test.Y!=knn.pred)
+mean(test.Y!="No")
+result = table(knn.pred, test.Y)
+result[2,2]/(result[2,1]+result[2,2])
+
+glm.fits = glm(Purchase~., data=Caravan, family = binomial, subset = -test)
+glm.probs = predict(glm.fits, Caravan[test,], type="response")
+glm.pred = rep("No",1000)
+glm.pred[glm.probs>0.25] = "Yes"
+table(glm.pred, test.Y)
+summary(test.Y)
+
+table(knn.pred,test.Y)
+head(test.Y)
+head(knn.pred)
--- SL/exercise.R
+++ SL/exercise.R
@@ -160,3 +160,128 @@ lm.medv <- (lm(crim~poly(medv,3)))
 par(mfrow=c(1,1))
 plot(crim~medv)
 points(medv, fitted(lm.medv),col="red",lwd=3)
+
+# Chapter 4 Exercise
+
+# Question 6
+exp(-6+0.05*40+1)/(1+exp(-6+0.05*40+1)) #a
+(log(1)+6-3.5)/0.05 #b
+
+# Question 7
+4*10/36 - (10^2)/(2*36) + log(0.8)
+
+# chapter 4
+# QUestion 10
+library(ISLR)
+library(MASS)
+?Weekly
+summary(Weekly)
+attach(Weekly)
+head(Weekly)
+names(Weekly)
+
+
+# a)
+cor(Weekly[,-9]) ; pairs(Weekly, col=Direction) # no significant pattern except between Year and Volume
+
+# b)
+glm.fit = glm(Direction~Lag1+Lag2+Lag3+Lag4+Lag5+Volume, family=binomial, data=Weekly)
+summary(glm.fit) # intercept and Lag2 are significant
+
+# c)
+glm.prob = predict(glm.fit, type="response")
+head(glm.prob)
+head(Weekly)
+
+        # what predict(glm.fit, type="response") really do: 
+        # probability = exp(linear function f) / (1 + exp(linear function f))
+        # linear function f use the coefficients from the logistic regression 
+x <- c(1, Lag1[1],Lag2[1],Lag3[1],Lag4[1],Lag5[1],Volume[1])
+b <- c(coef(glm.fit))
+f <- sum(x*b)
+exp(f)/(1+exp(f)) # same as glm.pprb = predict(glm.fit, type="response")
+
+glm.pred = rep("Down",nrow(Weekly))
+glm.pred[glm.prob>0.5] = "Up"
+
+table(glm.pred, Direction)
+1 - (54+557)/(48+430+54+557) # training error rate = sum of correct divided by total number of observations
+1 - mean(glm.pred == Weekly$Direction) # training error rate
+
+# d)
+train = Year<2009
+length(train)
+glm.fit = glm(Direction~Lag2, data=Weekly, family=binomial, subset=train)
+summary(glm.fit)
+glm.prob = predict(glm.fit, newdata=Weekly[!train,], type="response")
+glm.pred = ifelse(glm.prob>0.5, "Up","Down")
+table(glm.pred, Direction[!train])
+mean(glm.pred==Direction[!train])
+
+# e)
+lda.fit = lda(Direction~Lag2, data=Weekly, subset=train)
+lda.pred = predict(lda.fit, newdata=Weekly[!train,])
+table(lda.pred$class, Weekly$Direction[!train])
+mean(lda.pred$class==Direction[!train]) # 0.625 corret predicition rate
+#         Down Up
+#  Down    9   5
+#  Up     34   56
+
+# f)
+qda.fit = qda(Direction~Lag2, data=Weekly, subset=train)
+qda.pred = predict(qda.fit, newdata=Weekly[!train,])
+table(qda.pred$class, Direction[!train])
+mean(qda.pred$class==Direction[!train]) # 0.586 corect prediction rate
+        # down up
+#  Down   0    0
+#  Up     43   61
+
+# g)
+library(class)
+set.seed(1)
+knn.pred = knn(as.matrix(Lag2[train]), as.matrix(Lag2[!train]), Weekly$Direction[train], k=1)
+table(knn.pred, Direction[!train])
+mean(knn.pred==Direction[!train])
+
+train = Year<2009
+set.seed(1)
+knn.train = knn(as.matrix(Lag2[train]), as.matrix(Lag2[train]), Weekly$Direction[train], k=1)
+table(knn.train, Direction[train])
+#knn.pred  Down  Up
+#   Down   21    30
+#   Up     22    31
+
+#   0.5 correct prediction rate
+
+# i)
+
+ # trial to calculate LD1 and posterior probability "by hand" based on e)
+test.down <- subset(Weekly[,"Lag2"],Direction=="Down" & Year<2009)
+test.up <- subset(Weekly[,"Lag2"],Direction=="Up" & Year<2009)
+test <- subset(Weekly[,"Lag2"], Year<2009)
+length(test.down) ; length(test.up); length(test)
+mean(test.down) ; mean(test.up) ; var.test <- (var(test.down) + var(test.up))*length(test)/(length(test)-2); var.test
+prior.down <- lda.fit$prior[1]; prior.up <- lda.fit$prior[2]
+test[28:38];Lag2[28:38]
+z.down <- test*mean(test.down)/var.test - mean(test.down)^2/(2*var.test) +  log(prior.down); head(z.down)
+z.up <- test*mean(test.up)/var.test - mean(test.up)^2/(2*var.test) +  log(prior.up); head(z.up)
+head(test.up/var.test); head(mean(test.up)^2); head(2*var.test); head(test); head(test.up)
+head(z.down); head(z.up)
+z <- cbind(z.down, z.up); head(z)
+z[29:30,]; data.frame(lda.pred)[29:30,]; Weekly[29:30,]; test[29:30]; mean(test.up); mean(test.down); var.test; prior.down; prior.up
+
+data.frame(lda.pred)[25:35,]
+-4.978*(-0.03568254)/10.22933 - (-0.03568254)^2/(2*10.22933) + log(0.4477157) #down
+-4.978*0.2603658/10.22933 - 0.2603658^2/(2*10.22933) + log(0.5522843) #up
+
+-2.292*(-0.03568254)/10.22933 - (-0.03568254)^2/(2*10.22933) + log(0.4477157) #down
+-2.292*0.2603658/10.22933 - 0.2603658^2/(2*10.22933) + log(0.5522843) #up
+f.down <- prior.down*exp(-(x-mean(test.down)^2)/2*var.test)
+f.up <- prior.up*exp(-(x-mean(test.up)^2)/2*var.test)
+p.down <- f.down/(f.down + f.up); p.down
+p.up <- f.up/(f.down + f.up) ; p.up
+subset(data.frame(lda.pred), class=="Down")
+predict(lda.fit)$x[1]
+lda.fit$scaling*Weekly[train,]
+
+scale
